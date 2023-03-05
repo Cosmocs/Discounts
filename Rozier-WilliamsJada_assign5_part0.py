9e4dbfba74672f353005fbcf848c8a9caf209ea6
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2023-03-05 12:58:18 -0500

    Update Rozier-WilliamsJada_assign5_part0.py

--- Rozier-WilliamsJada_assign5_part0.py
+++ Rozier-WilliamsJada_assign5_part0.py
@@ -1,11 +1,12 @@
 number_of_prices = int(input("How many prices would you like to collect? "))
 if number_of_prices < 0:   
         print("Must be positive, try again")
-        print(number_of_prices)
+        print(number_of_prices)      
 
 
-price = input("Enter Price ")
+for prices in range(number_of_prices):
+    price = float(input("Enter Price "))
 
+  
 
-for i in range(number_of_prices):
-    print(price)
+subtotal =  
